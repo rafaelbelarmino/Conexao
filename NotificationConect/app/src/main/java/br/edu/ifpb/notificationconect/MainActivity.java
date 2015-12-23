0fe81a8795ffafee3b0372e600e1ccf80f652243
rafaelbelarmino <rafaelon@live.com>
2015-12-23 18:35:38 -0300

    editando usuar. adc. p

--- NotificationConect/app/src/main/java/br/edu/ifpb/notificationconect/MainActivity.java
+++ NotificationConect/app/src/main/java/br/edu/ifpb/notificationconect/MainActivity.java
@@ -8,14 +8,53 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class Logar extends Activity {
+    private EditText editUsu, editSenha;
+    private AlertDialog.Builder alert;
+    private Button Acessar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button notificacaoButton = (Button)findViewById(R.id.notificationButton);
+        etUsuario= (EditText) findviewById(R.id.editUsuario);
+        etSenha= (EditText) findviewById(R.id.editSenha);
+        btAcessar=(Button).findviewById(R.id.btAcessar);
+
+        btAcessar.setOnClickListener(new View.OnClickListener()
+
+
+    }
+
+    public void onClick(View v) {
+        String urlPost="http://localhost/MainActivity.java"
+        String urlGet="http://localhost/conexao/MainActivity.java?Usuario="+etUsuario.getText().toString()+"&senha="+etSenha
+         ArrayList<NameValuePair>  parametrosPost = new ArrayList<NameValuePair>();
+        parametrosPost.add(new BasicNameValuePair("usuario",etUsuario.get.Text().toString()));
+        parametrosPost.add(new BasicNameValuePair("senha",etSenha.get.Text().toString()));
+        String respostaRetornada = null;
+        try {
+            respostaRetornada=  HttpService.executaHttpPost(urlPost, parametrosPost);
+            String resposta = respostaRetornada.toString();
+            resposta = resposta.replaceAll("\\s+","");
+            if(resposta.equals("1"))
+            Toast.makeText(Logar.this, "Usuario Valido Parabéns ", Toast.LENGHT_LONG);
+            else
+                Toast.makeText(Logar.this, "Erro: Usuarui Invalido " + erro, Toast.LENGHT_LONG
+
+        }
+         catch(Exception erro) {
+             Toast.makeText(Logar.this, "Erro: ", Toast.LENGHT_LONG);
+
+
+         }
+
+
+    }
+
+    Button notificacaoButton = (Button)findViewById(R.id.notificationButton);
         notificacaoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
